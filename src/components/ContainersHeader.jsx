28e66c1d54bdbf2a176c68c65bbc957f27b2d159
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-03-25 22:08:01 +0400

    updated and finished ContainersHeader component

--- src/components/ContainersHeader.jsx
+++ src/components/ContainersHeader.jsx
@@ -4,18 +4,87 @@ import styled from "styled-components";
 
 export default function ContainersHeader() {
   return (
-    <Box>
-      <img src={ProfileImage} alt="" />
-    </Box>
+    <MainContainer>
+      <div className="top-side">
+        <ImageBox>
+          <img src={ProfileImage} alt="" />
+        </ImageBox>
+      </div>
+
+      <div className="bottom-side">
+        <TitleAndDate>
+          <span className="title">The Octocat </span>
+          <span className="date">Joined 25 Jan 2011</span>
+        </TitleAndDate>
+
+        <LinkBio>
+          <span className="link">octocat</span>
+          <span className="bio">This profile has no bio</span>
+        </LinkBio>
+      </div>
+    </MainContainer>
   );
 }
+const MainContainer = styled.div`
+  display: flex;
 
-const Box = styled.div`
-
+  .top-side {
+  }
+`;
+const ImageBox = styled.div`
+  display: inline;
   img {
     width: 7.3125rem;
     height: 7.3125rem;
     flex-shrink: 0;
     border-radius: 50%;
+  }
+`;
+
+const TitleAndDate = styled.div`
+  margin-left: 2.31rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 93%;
+
+  .title {
+    color: #2b3442;
+    font-size: 1.625rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+
+  .date {
+    color: #697c9a;
+    text-align: right;
+    font-size: 0.9375rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+`;
+const LinkBio = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2.31rem;
+
+  .link {
+    color: #0079ff;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-bottom: 1.25rem;
+    margin-top: 0.12rem;
+  }
+  .bio {
+    width: 30rem;
+    color: #4b6a9b;
+    font-size: 0.9375rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 166.667%;
   }
 `;
